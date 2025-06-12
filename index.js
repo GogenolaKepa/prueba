1a1ffa649a02e581d327c0e74846ddc9ad5c0df8
Kepa Gogenola <Kepa.Gogenola@alumnos.uai.edu.ar>
2025-06-11 21:53:29 -0300

    EjerciciosClase8

---
+++ index.js
@@ -0,0 +1,27 @@
+/* 'use strict' te mata el body abajo si no esta declarado */
+console.log('llegue');
+console.log('llegue');
+console.log('llegue');
+console.log('llegue');
+console.log('llegue');
+
+var body = document.getElementById('body');
+body.addEventListener('mousemove', function(e){
+    var xMove = e.offsetX;
+    var yMove = e.offsetY;
+    /*console.log(rgb('+xMove+','+yMove+','+yMove+'));
+
+    body.style.backgroundColor = 'rgb('+xMove+','+yMove+','+yMove+')' */
+})
+
+console.log(typeof a);
+var a = 'aa';
+
+var a;
+console.log(a === null);
+
+if(a){
+    console.log('tiene dato');
+    }else{
+        console.log('no tiene dato');
+}
